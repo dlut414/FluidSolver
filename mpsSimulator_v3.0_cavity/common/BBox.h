f7d26bbe3259201b8c260b76cc3fa2e96c8e3303
dlut414 <hfy8373@gmail.com>
2015-09-03 22:12:08 +0900

    change source
    add particle_x
    change BBox

--- mpsSimulator_v3.0_cavity/common/BBox.h
+++ mpsSimulator_v3.0_cavity/common/BBox.h
@@ -10,61 +10,50 @@
 
 #include <limits>
 #include <algorithm>
-#include "Vec3.h"
+#include <Eigen/Dense>
 
-template <typename real>
-class BBox
-{
-    typedef Vec3<real> vec;
+template <typename R, int D = 2>
+class BBox {
+	typedef Eigen::Matrix<R, D, 1> vec;
 public:
-    BBox()
-    {
-        real _infinity = std::numeric_limits<real>::infinity();
-
-        pMin = vec( _infinity,  _infinity,  _infinity);
-        pMax = vec(-_infinity, -_infinity, -_infinity);
+    BBox() {
+        R _infinity = std::numeric_limits<R>::infinity();
+		for (int i = 0; i < D; i++) {
+			pMin[i] = _infinity;
+			pMax[i] = -_infinity;
+		}
     }
 
 	template <typename T>
-	BBox(const BBox<T>& b) 
-	{
-		pMax = Vec3<real>(b.pMax);
-		pMin = Vec3<real>(b.pMin);
+	BBox(const BBox<T, D>& b) {
+		for (int i = 0; i < D; i++) {
+			pMin[i] = static_cast<R>(b.pMin[i]);
+			pMax[i] = static_cast<R>(b.pMax[i]);
+		}
 	}
 
-    BBox(const vec &p) : pMin(p), pMax(p)
-    {}
+    BBox(const vec& p) {
+		for (int i = 0; i < D; i++) {
+			pMin[i] = p[i];
+			pMax[i] = p[i];
+		}
+	}
 
-    BBox(const vec &p1, const vec &p2)
-    {
-        pMin = vec(std::min(p1.x, p2.x), std::min(p1.y, p2.y), std::min(p1.z, p2.z));
-        pMax = vec(std::max(p1.x, p2.x), std::max(p1.y, p2.y), std::max(p1.z, p2.z));
+    BBox(const vec& p1, const vec& p2) {
+		for (int i = 0; i < D; i++) {
+			pMin[i] = std::min(p1[i], p2[i]);
+			pMax[i] = std::max(p1[i], p2[i]);
+		}
     }
 
-    void operator+=(const vec &p)
-    {
-        pMin.x = std::min(pMin.x, p.x);
-        pMin.y = std::min(pMin.y, p.y);
-        pMin.z = std::min(pMin.z, p.z);
-        pMax.x = std::max(pMax.x, p.x);
-        pMax.y = std::max(pMax.y, p.y);
-        pMax.z = std::max(pMax.z, p.z);
+    void operator+=(const vec& p) {
+		for (int i = 0; i < D; i++) {
+			pMin[i] = std::min(pMin[i], p[i]);
+			pMax[i] = std::max(pMax[i], p[i]);
+		}
     }
 
-    BBox Union(const BBox &b, const vec &p) const
-    {
-        BBox ret = b;
-        ret.pMin.x = std::min(b.pMin.x, p.x);
-        ret.pMin.y = std::min(b.pMin.y, p.y);
-        ret.pMin.z = std::min(b.pMin.z, p.z);
-        ret.pMax.x = std::max(b.pMax.x, p.x);
-        ret.pMax.y = std::max(b.pMax.y, p.y);
-        ret.pMax.z = std::max(b.pMax.z, p.z);
-        return ret;
-    }
-
-    BBox Union(const BBox &b1, const BBox &b2) const
-    {
+    BBox Union(const BBox& b1, const BBox &b2) const {
         BBox ret = b1;
         ret.pMin.x = std::min(b1.pMin.x, b2.pMin.x);
         ret.pMin.y = std::min(b1.pMin.y, b2.pMin.y);
@@ -75,28 +64,24 @@ public:
         return ret;
     }
 
-    bool bOverlaps(const BBox &b) const
-    {
+    bool bOverlaps(const BBox &b) const {
         bool x = (pMax.x >= b.pMin.x) && (pMin.x <= b.pMax.x);
         bool y = (pMax.y >= b.pMin.y) && (pMin.y <= b.pMax.y);
         bool z = (pMax.z >= b.pMin.z) && (pMin.z <= b.pMax.z);
         return (x && y && z);
     }
 
-    bool bInside(const vec &p) const
-    {
+    bool bInside(const vec &p) const {
         return (p.x >= pMin.x && p.x <=pMax.x &&
                 p.y >= pMin.y && p.y <=pMax.y &&
                 p.z >= pMin.z && p.z <=pMax.z);
     }
 
-    vec pCenter() const
-    {
+    vec pCenter() const {
         return vec( (pMax.x+pMin.x), (pMax.y+pMin.y), (pMax.z+pMin.z) ) * 0.5f;
     }
 
-    void Expand(const real& s)
-    {
+    void Expand(const real& s) {
         vec v = s * (pMax - pMin);
         pMax += v;
         pMin -= v;
