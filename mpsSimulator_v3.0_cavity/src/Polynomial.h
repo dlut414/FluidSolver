952ce7070e706b9fd3de066a6ca10a1b9963d608
dlut414 <hfy8373@gmail.com>
2015-10-17 21:30:20 +0900

    modified Polynomial

--- mpsSimulator_v3.0_cavity/src/Polynomial.h
+++ mpsSimulator_v3.0_cavity/src/Polynomial.h
@@ -11,14 +11,17 @@ namespace SIM {
 
 	using namespace mMath;
 
-	template <unsigned D, unsigned P, unsigned I, unsigned J, unsigned HoldOrder, bool LastBit>
-	struct Multiset {};
+	template <unsigned D, unsigned J, unsigned ROrder> struct Repetition { enum { value = H<D-j-1,m>, }; };
 
-	template <unsigned D, unsigned P, unsigned I, unsigned J, unsigned HoldOrder, 1>	struct Multiset { enum { value = Multiset<D, P, I-1, J, HoldOrder, 0>::value-1; }; };
-	template <unsigned D, unsigned P, unsigned I, unsigned J, unsigned HoldOrder, 0>	struct Multiset { enum { value = Multiset<D, P, I-1, J, HoldOrder, 0>::value; }; };
+	template <unsigned D, unsigned I, unsigned J, unsigned ROrder, bool Clear>	struct CurRepetition						{ enum { value, }; };
+	template <unsigned D, unsigned J, unsigned ROrder, bool Clear>				struct CurRepetition <D,0,J,ROrder,Clear>	{ enum { value = 1, }; };
+	template <unsigned D, unsigned I, unsigned J, unsigned ROrder>				struct CurRepetition <D,I,J,ROrder,1> { enum { value = 1, }; };
+	template <unsigned D, unsigned I, unsigned J, unsigned ROrder>				struct CurRepetition <D,I,J,ROrder,0> { enum { value = CurRepetition<D,I-1,J,ROrder,CurRepetition<D,I-1,J,ROrder,0>::value>Repetition<D,J,ROrder>::value>::value + 1, }; };
 
-	template<unsigned D, unsigned P, 0, unsigned J, unsigned HoldOrder, bool LastBit>	struct Multiset { enum { value = 0, }; };
-	template<unsigned D, unsigned P, 0, 0, unsigned HoldOrder, bool LastBit>			struct Multiset { enum { value = P, }; };
+	template <unsigned D, unsigned P, unsigned I, unsigned J, unsigned ROrder, bool Rep> struct Multiset { enum { value, }; };
+
+	template<unsigned D, unsigned P, 0, unsigned J, unsigned ROrder>	struct Multiset { enum { value = 0, }; };
+	template<unsigned D, unsigned P, 0, 0, unsigned ROrder>			struct Multiset { enum { value = P, }; };
 
 	template <typename R, unsigned D, unsigned P>
 	class Polynomial_A {
--- mpsSimulator_v3.0_cavity/src/mMath.h
+++ mpsSimulator_v3.0_cavity/src/mMath.h
@@ -6,19 +6,10 @@
 
 namespace mMath {
 
-	template <unsigned n>
-	struct factorial {
-		enum { value = n* factorial<n - 1>::value, };
-	};
-
-	template <>
-	struct factorial < 1 > {
-		enum { value = 1, };
-	};
+	template <unsigned n>	struct factorial { enum { value = n* factorial<n - 1>::value, }; };
+	template <>				struct factorial<1> { enum { value = 1, }; };
 
 	template <unsigned n, unsigned m>
-	struct H {
-		enum { value = factorial<n + m - 1>::value / (factorial<m>::value * factorial<n - 1>::value), };
-	};
+	struct H { enum { value = factorial<n + m - 1>::value / (factorial<m>::value * factorial<n - 1>::value), };	};
 
 }
