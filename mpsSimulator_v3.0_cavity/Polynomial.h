c1084468c15958419d97ccb8391941571473d98f
dlut414 <hfy8373@gmail.com>
2015-10-26 19:24:51 +0900

    finish 2nd-order Polynomial_A

--- mpsSimulator_v3.0_cavity/Polynomial.h
+++ mpsSimulator_v3.0_cavity/Polynomial.h
@@ -32,10 +32,16 @@ namespace SIM {
 
 	template <typename R, unsigned P>				class Polynomial_A<R,2,P> : public Polynomial_A_<R,2,P> {
 	private:
-		template <unsigned P_ = 1, unsigned PX = P_, unsigned I = 0>	static __forceinline void Gen_		(const R* const x, R* const out) { out[I] = Power<PX>(x[0])*Power<P_-PX>(x[1]); Gen_<P_+1>(x, out); }
-		template <>										static __forceinline void Gen_<P>	(const R* const x, R* const out) { out[P-1] = Power<P>(x); }
+		template <unsigned P_ = 1, unsigned PX = P_, unsigned I = 0>
+		struct Gen__			{ static __forceinline void Gen(const R* const x, R* const out) { out[I] = Power<PX>(x[0])*Power<P_-PX>(x[1]); Gen__<P_,PX-1,I+1>::Gen(x, out); } };
+		template <unsigned P_, unsigned I>						
+		struct Gen__<P_,0,I>	{ static __forceinline void Gen(const R* const x, R* const out) { out[I] = Power<P_>(x[1]); } };
+		template <unsigned P_ = 1, unsigned I = 0, bool Over = !(P-P_)>
+		struct Gen_				{ static __forceinline void Gen(const R* const x, R* const out) { Gen__<P_,P_,I>::Gen(x, out); Gen_<P_+1,I+H<2,P_>::value>::Gen(x, out); } };
+		template <unsigned P_, unsigned I>
+		struct Gen_<P_,I,true>	{ static __forceinline void Gen(const R* const x, R* const out) { Gen__<P_,P_,I>::Gen(x, out); } };
 	public:
-		static __forceinline void Gen(const R* const x, R* const out) { Gen_(x, out); }
+		static __forceinline void Gen(const R* const x, R* const out) { Gen_<>::Gen(x, out); }
 	};
 	
 
