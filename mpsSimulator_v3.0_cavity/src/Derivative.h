5a8dca888edd7b14dae2efa6e38b5f83ee9179d0
dlut414 <hfy8373@gmail.com>
2015-10-29 19:38:32 +0900

    add Derivative_A 2D

--- mpsSimulator_v3.0_cavity/src/Derivative.h
+++ mpsSimulator_v3.0_cavity/src/Derivative.h
@@ -13,64 +13,75 @@ namespace mMath {
 	template <typename R, unsigned D, unsigned P>
 	class Derivative_A_ {
 	public:
-		template <unsigned N>	static __forceinline R Power(const R& x)	{ return x*Power<N - 1>(x); }
+		template <unsigned N>	static __forceinline R Power(const R& x)	{ return x*Power<N-1>(x); }
 		template <>				static __forceinline R Power<0>(const R& x) { return 1; }
+		template <unsigned N, unsigned M>	static __forceinline R A() { return (factorial<N>::value) / (factorial<N-M>::value); }
 	};
 
-	template <typename R, unsigned D, unsigned P>	class Derivative_A : public Derivative_A_<R,D,P>{};
+	template <typename R, unsigned D, unsigned P>	class Derivative_A : public Derivative_A_<R,D,P> {};
 
-	template <typename R, unsigned P>				class Derivative_A<R,1,P> : public Derivative_A_<R,1,P> {
+	template <typename R, unsigned P>				class Derivative_A<R,1,P> : public Derivative_A_<R,1,P>{
 	private:
-		template <unsigned Der, unsigned P_ = 1, bool Zero = (Der>P_) > 
-		struct Gen_					{
-			static __forceinline void Gen(const R& x, R* const out)				{ out[P_-1] = Der*Power<P_-Der>(x); Gen_<Der,P_+1,(Der>(P_+1))>::Gen(x, out); } 
-			static __forceinline void Gen(const R& s, const R& x, R* const out) { out[P_-1] = Der*Power<P_-Der>(x/s)*Power<Der>(1/s); Gen_<Der,P_+1,(Der>(P_+1))>::Gen(s, x, out); }
+		template <unsigned Der, unsigned P_ = 1, bool Zero = (Der>P_), bool Over = (P_==P)>
+		struct Gen_ {
+			static __forceinline void Gen(const R& x, R* const out)				{ out[P_-1] = A<P_,Der>()*Power<P_-Der>(x); Gen_<Der,P_+1,(Der>(P_+1))>::Gen(x, out); }
+			static __forceinline void Gen(const R& s, const R& x, R* const out) { out[P_-1] = A<P_,Der>()*Power<P_-Der>(x/s)*Power<Der>(1/s); Gen_<Der,P_+1,(Der>(P_+1))>::Gen(s, x, out); }
+		};
+		template <unsigned Der, unsigned P_, bool Over>
+		struct Gen_<Der,P_,true,Over> {
+			static __forceinline void Gen(const R& x, R* const out)				{ out[P_-1] = 0; Gen_<Der,P_+1,(Der>(P_+1))>::Gen(x, out); }
+			static __forceinline void Gen(const R& s, const R& x, R* const out) { out[P_-1] = 0; Gen_<Der,P_+1, (Der>(P_+1))>::Gen(s, x, out); }
 		};
 		template <unsigned Der, unsigned P_>
-		struct Gen_<Der,P_,true>		{
-			static __forceinline void Gen(const R& x, R* const out)				{ out[P_-1] = 0; Gen_<Der,P_+1,(Der>(P_+1))>::Gen(x, out); }
-			static __forceinline void Gen(const R& s, const R& x, R* const out) { out[P_-1] = 0; Gen_<Der,P_+1,(Der>(P_+1))>::Gen(s, x, out); }
-		};
-		template <unsigned Der>
-		struct Gen_<Der,P,true>		{
+		struct Gen_<Der,P_,true,true> {
 			static __forceinline void Gen(const R& x, R* const out)				{ out[P_-1] = 0; }
 			static __forceinline void Gen(const R& s, const R& x, R* const out) { out[P_-1] = 0; }
 		};
-		template <unsigned Der>
-		struct Gen_<Der,P,false>		{
-			static __forceinline void Gen(const R& x, R* const out) { out[P_-1] = Der*Power<P_-Der>(x); }
-			static __forceinline void Gen(const R& s, const R& x, R* const out) { out[P_-1] = Der*Power<P_-Der>(x/s)*Power<Der>(1/s); }
+		template <unsigned Der, unsigned P_>
+		struct Gen_<Der,P_,false,true> {
+			static __forceinline void Gen(const R& x, R* const out)				{ out[P_-1] = A<P_,Der>()*Power<P_-Der>(x); }
+			static __forceinline void Gen(const R& s, const R& x, R* const out) { out[P_-1] = A<P_,Der>()*Power<P_-Der>(x/s)*Power<Der>(1/s); }
 		};
 	public:
-		template <unsigned Der> static __forceinline void Gen(const R& x, R* const out)				{ Gen_<Der>::Gen(x, out); }
+		template <unsigned Der> static __forceinline void Gen(const R& x, R* const out) { Gen_<Der>::Gen(x, out); }
 		template <unsigned Der> static __forceinline void Gen(const R& s, const R& x, R* const out) { Gen_<Der>::Gen(s, x, out); }
 	};
 
 	template <typename R, unsigned P>				class Derivative_A<R,2,P> : public Derivative_A_<R,2,P> {
 	private:
-		template <unsigned P_ = 1, unsigned PX = P_, unsigned I = 0>
-		struct Gen__			{ 
-			static __forceinline void Gen(const R* const x, R* const out)				{ out[I] = Power<PX>(x[0])*Power<P_-PX>(x[1]); Gen__<P_,PX-1,I+1>::Gen(x, out); } 
-			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ out[I] = Power<PX>(x[0]/s)*Power<P_-PX>(x[1]/s); Gen__<P_,PX-1,I+1>::Gen(s, x, out); }
+		template <unsigned DerX, unsigned DerY, unsigned P_ = 1, unsigned PX = P_, unsigned I = 0, bool Zero = (DerX>PX||DerY>(P_-PX))>
+		struct Gen__									{ 
+			static __forceinline void Gen(const R* const x, R* const out)				{ out[I] = A<PX,DerX>()*A<P_-PX,DerY>()*Power<PX-DerX>(x[0])*Power<P_-PX-DerY>(x[1]); Gen__<DerX,DerY,P_,PX-1,I+1>::Gen(x, out); } 
+			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ out[I] = A<PX,DerX>()*A<P_-PX,DerY>()*Power<DerX+DerY>(1/s)*Power<PX-DerX>(x[0]/s)*Power<P_-PX-DerY>(x[1]/s); Gen__<DerX,DerY,P_,PX-1,I+1>::Gen(s, x, out); }
 		};
-		template <unsigned P_, unsigned I>						
-		struct Gen__<P_,0,I>	{ 
-			static __forceinline void Gen(const R* const x, R* const out)				{ out[I] = Power<P_>(x[1]); }
-			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ out[I] = Power<P_>(x[1]/s); }
+		template <unsigned DerX, unsigned DerY, unsigned P_, unsigned PX, unsigned I>
+		struct Gen__ <DerX,DerY,P_,PX,I,true>			{
+			static __forceinline void Gen(const R* const x, R* const out)				{ out[I] = 0; Gen__<DerX,DerY,P_,PX-1,I+1>::Gen(x, out); } 
+			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ out[I] = 0; Gen__<DerX,DerY,P_,PX-1,I+1>::Gen(s, x, out); }
 		};
-		template <unsigned P_ = 1, unsigned I = 0, bool Over = !(P-P_)>
-		struct Gen_				{ 
-			static __forceinline void Gen(const R* const x, R* const out)				{ Gen__<P_,P_,I>::Gen(x, out); Gen_<P_+1,I+H<2,P_>::value>::Gen(x, out); } 
-			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ Gen__<P_,P_,I>::Gen(s, x, out); Gen_<P_+1,I+H<2,P_>::value>::Gen(s, x, out); }
+		template <unsigned DerX, unsigned DerY, unsigned P_, unsigned I>
+		struct Gen__<DerX,DerY,P_,0,I,true> 			{
+			static __forceinline void Gen(const R* const x, R* const out)				{ out[I] = 0; }
+			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ out[I] = 0; }
 		};
-		template <unsigned P_, unsigned I>
-		struct Gen_<P_,I,true>	{ 
-			static __forceinline void Gen(const R* const x, R* const out)				{ Gen__<P_,P_,I>::Gen(x, out); } 
-			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ Gen__<P_,P_,I>::Gen(s, x, out); }
+		template <unsigned DerX, unsigned DerY, unsigned P_, unsigned I>
+		struct Gen__<DerX,DerY,P_,0,I,false> 			{
+			static __forceinline void Gen(const R* const x, R* const out)				{ out[I] = A<0,DerX>()*A<P_-0,DerY>()*Power<0-DerX>(x[0])*Power<P_-0-DerY>(x[1]); }
+			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ out[I] = A<0,DerX>()*A<P_-0,DerY>()*Power<DerX+DerY>(1/s)*1*Power<P_-0-DerY>(x[1]/s); }
+		};
+		template <unsigned DerX, unsigned DerY, unsigned P_ = 1, unsigned I = 0, bool Over = (P_==P)>
+		struct Gen_							{ 
+			static __forceinline void Gen(const R* const x, R* const out)				{ Gen__<DerX,DerY,P_,P_,I>::Gen(x, out); Gen_<DerX,DerY,P_+1,I+H<2,P_>::value>::Gen(x, out); } 
+			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ Gen__<DerX,DerY,P_,P_,I>::Gen(s, x, out); Gen_<DerX,DerY,P_+1,I+H<2,P_>::value>::Gen(s, x, out); }
+		};
+		template <unsigned DerX, unsigned DerY, unsigned P_, unsigned I>
+		struct Gen_<DerX,DerY,P_,I,true>	{ 
+			static __forceinline void Gen(const R* const x, R* const out)				{ Gen__<DerX,DerY,P_,P_,I>::Gen(x, out); } 
+			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ Gen__<DerX,DerY,P_,P_,I>::Gen(s, x, out); }
 		};
 	public:
-		static __forceinline void Gen(const R* const x, R* const out)				{ Gen_<>::Gen(x, out); }
-		static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ Gen_<>::Gen(s, x, out); }
+		template <unsigned DerX, unsigned DerY>	static __forceinline void Gen(const R* const x, R* const out) { Gen_<DerX,DerY>::Gen(x, out); }
+		template <unsigned DerX, unsigned DerY>	static __forceinline void Gen(const R& s, const R* const x, R* const out) { Gen_<DerX,DerY>::Gen(s, x, out); }
 	};
 	
 
