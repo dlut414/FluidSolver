72e688c4f60705eaf7daeb9cafe3aaeb744f5653
dlut414 <hfy8373@gmail.com>
2015-11-06 18:15:13 +0900

    Passed qualitative test
    Major change

--- mpsSimulator_v3.0_cavity/src/Polynomial.h
+++ mpsSimulator_v3.0_cavity/src/Polynomial.h
@@ -23,8 +23,8 @@ namespace mMath {
 	private:
 		template <unsigned P_ = 1>	static __forceinline void Gen_		(const R& x, R* const out) { out[P_-1] = Power<P_>(x); Gen_<P_+1>(x, out); }
 		template <>					static __forceinline void Gen_<P>	(const R& x, R* const out) { out[P-1] = Power<P>(x); }
-		template <unsigned P_ = 1>	static __forceinline void Gen_		(const R& s, const R& x, R* const out) { out[P_-1] = Power<P_>(x/s); Gen_<P_+1>(s, x, out); }
-		template <>					static __forceinline void Gen_<P>	(const R& s, const R& x, R* const out) { out[P-1] = Power<P>(x/s); }
+		template <unsigned P_ = 1>	static __forceinline void Gen_		(const R& s, const R& x, R* const out) { out[P_-1] = Power<P_>(s*x); Gen_<P_+1>(s, x, out); }
+		template <>					static __forceinline void Gen_<P>	(const R& s, const R& x, R* const out) { out[P-1] = Power<P>(s*x); }
 	public:
 		static __forceinline void Gen(const R& x, R* const out)				{ Gen_(x, out); }
 		static __forceinline void Gen(const R& s, const R& x, R* const out) { Gen_(s, x, out); }
@@ -35,12 +35,12 @@ namespace mMath {
 		template <unsigned P_ = 1, unsigned PX = P_, unsigned I = 0>
 		struct Gen__			{ 
 			static __forceinline void Gen(const R* const x, R* const out)				{ out[I] = Power<PX>(x[0])*Power<P_-PX>(x[1]); Gen__<P_,PX-1,I+1>::Gen(x, out); } 
-			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ out[I] = Power<PX>(x[0]/s)*Power<P_-PX>(x[1]/s); Gen__<P_,PX-1,I+1>::Gen(s, x, out); }
+			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ out[I] = Power<PX>(s*x[0])*Power<P_-PX>(s*x[1]); Gen__<P_,PX-1,I+1>::Gen(s, x, out); }
 		};
 		template <unsigned P_, unsigned I>						
 		struct Gen__<P_,0,I>	{ 
 			static __forceinline void Gen(const R* const x, R* const out)				{ out[I] = Power<P_>(x[1]); }
-			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ out[I] = Power<P_>(x[1]/s); }
+			static __forceinline void Gen(const R& s, const R* const x, R* const out)	{ out[I] = Power<P_>(s*x[1]); }
 		};
 		template <unsigned P_ = 1, unsigned I = 0, bool Over = (P==P_)>
 		struct Gen_				{ 
