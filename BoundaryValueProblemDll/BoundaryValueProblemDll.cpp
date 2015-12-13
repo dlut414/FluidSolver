9ac885f18acf0ca5eb46c980e3e4ba43808800a7
dlut414 <hfy8373@gmail.com>
2015-12-14 00:27:12 +0900

    Major change
    add new projects: PassiveScalarProblem ...

--- BoundaryValueProblemDll/BoundaryValueProblemDll.cpp
+++ BoundaryValueProblemDll/BoundaryValueProblemDll.cpp
@@ -3,8 +3,4 @@
 
 #include "stdafx.h"
 
-int _tmain(int argc, _TCHAR* argv[])
-{
-	return 0;
-}
 
---
+++ VisualizationDll/VisualizationDll.cpp
@@ -0,0 +1,6 @@
+// Visualization.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+
+
